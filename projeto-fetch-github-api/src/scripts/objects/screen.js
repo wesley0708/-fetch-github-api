9a849031963721d25cc61501e2c6e5c63856aa78
Wesley <wesley22335@gmail.com>
2025-05-18 19:30:21 -0300

    ptimeriro commit

---
+++ projeto-fetch-github-api/src/scripts/objects/screen.js
@@ -0,0 +1,27 @@
+const screen = {
+    userProfile: document.querySelector('.profile-data'),
+    renderUser(user) {
+        this.userProfile.innerHTML = `<div class="info">
+                   <img src="${user.avatarUrl}" alt="Foto do perfil do usuario" /> 
+                      <div class="data"> 
+                          <h1>${user.name ?? 'Não possui nome cadastrado 😥'}</h1>
+                          <p>${user.bio ?? 'Não possui bio cadastrada  😥'}</p>
+                          </div>
+                      </div>`
+
+        let repositoriesItems = ''  
+        user.repositories.forEach(repo => repositoriesItems+= `<li><a href="${repo.html_url}"
+            target="_blank">${repo.name}</a></li>`)
+          
+        if(user.repositories.length > 0) {
+            this.userProfile.innerHTML += `<div class="repositories section">
+                                           <h2>Repositórios</h2>
+                                           <ul>${repositoriesItems}</ul>
+                                            </div>` 
+        }           
+    },
+    renderNotFound() {
+        this.userProfile.innerHTML = "<h3> Usuário não encontrado 😥</h3>"
+    }
+}
+export { screen }
